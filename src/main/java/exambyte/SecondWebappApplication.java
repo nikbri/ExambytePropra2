44f48b13ba85b47d04de4f1611019dc33cbcd2fa
Niklas Brinkmann <Niklas.Brinkmann@hhu.de>
2024-10-25 14:42:46 +0200

    Initial commit Gradle und Spring Boot web sind integriert

---
+++ src/main/java/exambyte/SecondWebappApplication.java
@@ -0,0 +1,13 @@
+package exambyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondWebappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SecondWebappApplication.class, args);
+  }
+
+}
