37c233d9f720ac1d177034482fe81166dcf357aa
Niklas Brinkmann <Niklas.Brinkmann@hhu.de>
2024-11-14 16:16:55 +0100

    Refactor Kommentar hinzugefügt

--- src/main/java/exambyte/User.java
+++ src/main/java/exambyte/User.java
@@ -3,6 +3,9 @@ package exambyte;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+
+/* Rekord dient erstmal nur, damit die Controller etc. programmiert werden können
+folgende Attribute müssen vermutlich noch hinzugefügt werden: Tests[] Liste mit allen Tests des Users*/
 public record User(
         @NotBlank(message = "Name darf nicht leer sein") String benutzername,
         @Email(message = "Bitte gültige Email angeben") Email email
