ade310eb5b360a95fd2d6e8d15976af4811afcc3
Niklas Brinkmann <Niklas.Brinkmann@hhu.de>
2024-11-19 13:16:00 +0100

    Add Konstruktor Für Testzwecke

--- src/main/java/exambyte/Users.java
+++ src/main/java/exambyte/Users.java
@@ -1,11 +1,21 @@
 package exambyte;
 
-import jakarta.validation.constraints.Email;
-
 import java.util.ArrayList;
 
 public class Users {
-    ArrayList<User> users;
+
+    ArrayList<User> users = new ArrayList<>();
+
+    //Konstruktor ist nur provisorisch, um einen default User zum testen zu haben...
+
+    /*Wusste es nicht anders zu lösen, auch wenn das natürlich nicht gut programmiert ist
+    wenn der arrange Schritt im produktiv code ist.
+    Bei Ideen gerne refactorn und melden (Niklas)*/
+    public Users() {
+        User defaultUser = new User("default", "default", "default");
+        users.add(defaultUser);
+    }
+
 
     public boolean userExists(String username, String password) {
         for (User user : users) {
@@ -16,9 +26,21 @@ public class Users {
         return false;
     }
 
-    public void addUser(String username, Email email, String password) {
+    public User getUser(String username){
+        for (User user : users) {
+            if(user.benutzername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void addUser(String username,String email, String password) {
         User user = new User(username, email, password);
         users.add(user);
         return;
+    }
+    public void addUser(User user) {
+        users.add(user);
     }
 }
