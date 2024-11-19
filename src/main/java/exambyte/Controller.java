7f220a9d7e8e6b3b983fabdfce8bacc454876d17
Niklas Brinkmann <Niklas.Brinkmann@hhu.de>
2024-11-19 13:15:25 +0100

    Add method login Controller Methode für die Login Seite. Cookies müssen noch hinzugefügt werden

--- src/main/java/exambyte/Controller.java
+++ src/main/java/exambyte/Controller.java
@@ -1,18 +1,40 @@
 package exambyte;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.boot.web.server.Cookie;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+
+    //Datenbank Zugriff auf die Users soll das darstellen, hab aber keine Ahnung
+    // wie sowas funktioniert.
+    Users users = new Users();
 
     @GetMapping("/")
     public String wellcome(){
         return "wellcome";
     }
 
-    @GetMapping("/login")
-    public String login(){
-        return "login";
+    @GetMapping("/studentLogin")
+    public String login(Model model,
+                        @RequestParam(defaultValue = "Benutzername") String benutzer,
+                        @RequestParam(defaultValue = "Passwort") String passwort) {
+        if(benutzer.equals("Benutzername") || passwort.equals("Passwort")){
+            model.addAttribute("benutzer", benutzer);
+            model.addAttribute("passwort", passwort);
+            return "studentLogin";
+        }
+        if (users.userExists(benutzer, passwort)) {
+            return "redirect:/Startseite";
+        }
+        model.addAttribute("benutzer", benutzer);
+        model.addAttribute("passwort", "Passwort");
+        return "studentLogin";
     }
 
 
--- src/test/java/exambyte/WebTests.java
+++ src/test/java/exambyte/WebTests.java
@@ -2,24 +2,59 @@ package exambyte;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import jakarta.validation.constraints.Email;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @WebMvcTest
 public class WebTests {
 
+
     @Autowired
     MockMvc mvc;
+
 
     @Test
     @DisplayName("Wellcome Seite erreichbar")
     public void test_welcome() throws Exception {
         mvc.perform(get("/"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Login ohne Parameter aufrufen")
+    public void test_login() throws Exception {
+        mvc.perform(get("/studentLogin"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Login User Exists")
+    public void test_login_exists() throws Exception {
+
+        mvc.perform(get("/studentLogin?benutzer=default&passwort=default"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/Startseite"));
+    }
+
+    @Test
+    @DisplayName("Login User do not Exists")
+    public void test_login_dont_exists() throws Exception {
+
+        mvc.perform(get("/studentLogin?benutzer=Name&passwort=default"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("studentLogin"))
+                .andExpect(model().attribute("benutzer", "Name"));
     }
 
 }
