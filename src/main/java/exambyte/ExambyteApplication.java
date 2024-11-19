f04c008304d45d588f619cf1a6a66f8f9ae4bd93
Catchme1fyoucan <david.birgel@gmx.de>
2024-11-20 00:31:10 +0100

    Umbennenung von WebappAplication in ExambyteApplication

--- src/main/java/exambyte/ExambyteApplication.java
+++ src/main/java/exambyte/ExambyteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecondWebappApplication {
+public class ExambyteApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SecondWebappApplication.class, args);
+    SpringApplication.run(ExambyteApplication.class, args);
   }
 
 }
